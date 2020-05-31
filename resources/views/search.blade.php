aa1c9910667a89e4847d613fb9f95d023262c508
klickers <bluebirid228@gmail.com>
2020-05-30 21:07:59 -0700

    removed $card conver to json (unnecessary)

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -86,9 +86,6 @@ Search {{ $word ?? '' }}
         <div id = "holder" class = "md:ml-6">
             <div id = "card-wrap" class = "card-group-wrapper mt-2">
                 @foreach ($cards as $card)
-                    @php
-                        $card = json_encode($card);
-                    @endphp
                     <x-card
                           width="full"
                           height="h-long"
