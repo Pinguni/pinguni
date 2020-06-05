8cbbee3919be3fe0d67d31fedee27be38c0e9d80
klickers <bluebirid228@gmail.com>
2020-06-05 13:40:57 -0700

    add VueJS

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,7 +18,8 @@ require('laravel-mix-purgecss');
  |
  */
 
-mix.sass('resources/sass/app.scss', 'public/css')
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
     .options({
         processCssUrls: false,
         postCss: [ tailwindcss() ],
