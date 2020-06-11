a9222fe491fdebeab4ba268a8d1adf92ca07c1ce
klickers <bluebirid228@gmail.com>
2020-06-10 17:12:30 -0700

    update edit/create card forms
    add Summernote, add tags placeholder, group form fields

--- resources/views/admin/create/card.blade.php
+++ resources/views/admin/create/card.blade.php
@@ -4,6 +4,9 @@
 
 @section('head')
     <link href = "/css/components/forms.css" rel = "stylesheet" />
+    <!-- Summernote -->
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
 @endsection
 
 
@@ -22,29 +25,31 @@
     
     <input type = "hidden" name = "parent_id" value = "{{ $parent_id ?? '' }}" />
     
-    <label for = "icon">Icon</label>
-    <input type = "text" name = "icon" placeholder = "icon" value="{{ old('icon') }}"/>
-    @error('icon')
-        <p role="alert">
-            <strong>{{ $message }}</strong>
-        </p>
-    @enderror
-    
-    <label for = "thumbnail">Thumbnail</label>
-    <input type = "text" name = "thumbnail" placeholder = "thumbnail" value="{{ old('thumbnail') }}"/>
-    @error('thumbnail')
-        <p role="alert">
-            <strong>{{ $message }}</strong>
-        </p>
-    @enderror
-    
-    <label for = "bg">Background</label>
-    <input type = "text" name = "bg" placeholder = "bg" value="{{ old('bg') }}"/>
-    @error('bg')
-        <p role="alert">
-            <strong>{{ $message }}</strong>
-        </p>
-    @enderror
+    <div class = "wrap-3">
+        <label for = "icon">Icon</label>
+        <input type = "text" name = "icon" placeholder = "icon" value="{{ old('icon') }}"/>
+        @error('icon')
+            <p role="alert">
+                <strong>{{ $message }}</strong>
+            </p>
+        @enderror
+
+        <label for = "thumbnail">Thumbnail</label>
+        <input type = "text" name = "thumbnail" placeholder = "thumbnail" value="{{ old('thumbnail') }}"/>
+        @error('thumbnail')
+            <p role="alert">
+                <strong>{{ $message }}</strong>
+            </p>
+        @enderror
+
+        <label for = "bg">Background</label>
+        <input type = "text" name = "bg" placeholder = "bg" value="{{ old('bg') }}"/>
+        @error('bg')
+            <p role="alert">
+                <strong>{{ $message }}</strong>
+            </p>
+        @enderror
+    </div>
     
     <label for = "title">Title</label>
     <input type = "text" name = "title" placeholder = "title" required value="{{ old('title') }}"/>
@@ -55,7 +60,7 @@
     @enderror
     
     <label for = "description">Description</label>
-    <textarea name = "description" required>{{ old('description') }}</textarea>
+    <textarea name = "description" required placeholder = "description">{{ old('description') }}</textarea>
     @error('description')
         <p role="alert">
             <strong>{{ $message }}</strong>
@@ -63,8 +68,7 @@
     @enderror
     
     <label for = "notes">Notes</label>
-    <div class = "editor language-js"></div>
-    <input name = "notes" id = "notes" type = "hidden" value="{{ old('notes') }}"/>
+    <textarea name = "notes" class = "summernote">{!! old('notes') !!}</textarea>
     @error('notes')
         <p role="alert">
             <strong>{{ $message }}</strong>
@@ -72,65 +76,61 @@
     @enderror
     
     <label for = "tags">Tags</label>
-    <textarea name = "tags">{{ old('tags') }}</textarea>
+    <textarea name = "tags" placeholder = "tags">{{ old('tags') }}</textarea>
     @error('tags')
         <p role="alert">
             <strong>{{ $message }}</strong>
         </p>
     @enderror
     
-    <label for = "type">Type</label>
-    <input type = "text" name = "type" placeholder = "type" required value="{{ old('type') }}"/>
-    @error('type')
-        <p role="alert">
-            <strong>{{ $message }}</strong>
-        </p>
-    @enderror
-    
-    <label for = "visibility">Visibility</label>
-    <input type = "text" name = "visibility" placeholder = "visibility" required value="{{ old('visibility') }}"/>
-    @error('visibility')
-        <p role="alert">
-            <strong>{{ $message }}</strong>
-        </p>
-    @enderror
-    
-    <label for = "permalink">Permalink</label>
-    <input type = "text" name = "permalink" placeholder = "permalink" required value="{{ old('permalink') }}"/>
-    @error('permalink')
-        <p role="alert">
-            <strong>{{ $message }}</strong>
-        </p>
-    @enderror
+    <div class = "wrap-3">
+        <label for = "type">Type</label>
+        <input type = "text" name = "type" placeholder = "type" required value="{{ old('type') }}"/>
+        @error('type')
+            <p role="alert">
+                <strong>{{ $message }}</strong>
+            </p>
+        @enderror
+
+        <label for = "visibility">Visibility</label>
+        <input type = "text" name = "visibility" placeholder = "visibility" required value="{{ old('visibility') }}"/>
+        @error('visibility')
+            <p role="alert">
+                <strong>{{ $message }}</strong>
+            </p>
+        @enderror
+
+        <label for = "permalink">Permalink</label>
+        <input type = "text" name = "permalink" placeholder = "permalink" required value="{{ old('permalink') }}"/>
+        @error('permalink')
+            <p role="alert">
+                <strong>{{ $message }}</strong>
+            </p>
+        @enderror
+    </div>
     
     <br />
     
     <button type = "submit">Create</button>
 </form>
 
+@endsection
 
-<script type="module">
-    // Code below from https://github.com/antonmedv/codejar/blob/master/index.html
-    
-    import { CodeJar } from 'https://medv.io/codejar/codejar.js'
-    
-    const editor = document.querySelector('.editor')
-    const highlight = editor => {
-        // highlight.js does not trim old tags,
-        // let's do it by this hack.
-        editor.textContent = editor.textContent
-        hljs.highlightBlock(editor)
-    }
-    const jar = CodeJar(editor, highlight)
-    jar.updateCode("{{ old('notes') }}")
-    
-    var notes = document.getElementById('notes')
-    
-    // Listen to updates
-    jar.onUpdate(code => {
-        notes.value = code;
-    });
+
+@section('scripts')
+<script>
+    $('.summernote').summernote({
+        tabsize: 2,
+        height: 120,
+        toolbar: [
+          ['style', ['style']],
+          ['font', ['bold', 'underline', 'clear']],
+          ['color', ['color']],
+          ['para', ['ul', 'ol', 'paragraph']],
+          ['table', ['table']],
+          ['insert', ['link', 'picture', 'video']],
+          ['view', ['fullscreen', 'codeview', 'help']]
+        ]
+      });
 </script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.1/highlight.min.js"></script>
-
 @endsection
