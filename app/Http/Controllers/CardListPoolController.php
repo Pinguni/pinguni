d8e2112e0ee1e89b1f405f10a49e61ce2d1d43ed
klickers <bluebirid228@gmail.com>
2020-07-29 16:25:34 -0700

    add new tables

---
+++ app/Http/Controllers/CardListPoolController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CardListPool;
+use Illuminate\Http\Request;
+
+class CardListPoolController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CardListPool  $cardListPool
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CardListPool $cardListPool)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CardListPool  $cardListPool
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CardListPool $cardListPool)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CardListPool  $cardListPool
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CardListPool $cardListPool)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CardListPool  $cardListPool
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CardListPool $cardListPool)
+    {
+        //
+    }
+}
